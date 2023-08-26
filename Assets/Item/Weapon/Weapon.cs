ee0922cfd21a18ca8e9f1b9f2ac6e23acde09ebe
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-08-26 21:20:39 +0700

    FEAT: Add Weapon category and Fire Wand Item

---
+++ Assets/Item/Weapon/Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    public ElementalType elementalType;
+    public float fireDelay;
+    virtual public void Attack(GameObject attacker){Debug.Log("Performed: Attack"); }
+    virtual public void HoldAttack(GameObject attacker){Debug.Log("Performed: HoldAttack");}
+}
