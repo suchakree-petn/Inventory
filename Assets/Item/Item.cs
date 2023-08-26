91eed4f37de512e5d5531e9dd2c4b359c55b6192
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-08-26 21:16:22 +0700

    FEAT: Add base Item class

---
+++ Assets/Item/Item.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Default,
+    Weapon,
+    Consumable,
+    Material
+}
+public enum ItemRarity
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
+public class Item : ScriptableObject
+{
+    [Header("Info")]
+    public string _name;
+    public string _description;
+    public Sprite icon;
+    public ItemType _itemType;
+    public ItemRarity _itemRarity;
+    public bool _isStackable;
+
+    [Header("Prefab")]
+    public GameObject itemPrefab;
+
+    void Awake()
+    {
+        _itemType = ItemType.Default;
+    }
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ElementalType
+    {
+        Fire,
+        Thunder,
+        Frost,
+        Wind
+
+    }
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
