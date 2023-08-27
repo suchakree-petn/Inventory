90625fe88f101e54ef1d89a751d3714a9f559829
suchakree-petn <suchakree.petn@kmutt.ac.th>
2023-08-27 16:51:35 +0700

    ADD: Add consumable and some Objects and Asset of Exp Scroll

---
+++ Assets/Item/Consumable/ExpScroll/ExpScroll.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Exp Scroll", menuName = "Item/Consumable/Exp Scroll")]
+public class ExpScroll : Consumable
+{
+    [SerializeField] private float _expAmount;
+
+    public override void Consume(GameObject consumer)
+    {
+        base.Consume(consumer);
+
+        // Add Exp to consumer
+    }
+}
